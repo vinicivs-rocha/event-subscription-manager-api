57581c4a6598ded91b50ec34d783600f07e786cd
vinicivs-rocha <vinicivsdev@gmail.com>
2025-04-29 15:48:41 -0300

    test: add event detailing by slug integration testing

---
+++ src/test/java/com/example/eventsubscriptionmanagerapi/integration/EventDetailingBySlugIntegrationTests.java
@@ -0,0 +1,73 @@
+package com.example.eventsubscriptionmanagerapi.integration;
+
+import com.example.eventsubscriptionmanagerapi.controllers.EventsController;
+import com.example.eventsubscriptionmanagerapi.dtos.ErrorMessageDTO;
+import com.example.eventsubscriptionmanagerapi.models.Event;
+import com.example.eventsubscriptionmanagerapi.repositories.EventRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
+class EventDetailingBySlugIntegrationTests {
+    @Container
+    static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    private EventsController eventsController;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgresqlContainer::getDriverClassName);
+    }
+
+    @AfterEach
+    void tearDown() {
+        eventRepository.deleteAll();
+    }
+
+    @Test
+    void shouldDetailEvent_whenThereIsAnEventWithTheSlug() {
+        var event = Event.builder().title("Code Commit 2024/02").address("Rua dos Bobos").price(100F).startsAt(LocalDate.now()).endsAt(LocalDate.now()).build();
+        eventRepository.save(event);
+
+        var response = eventsController.detailBySlug(event.getSlug());
+        var responseBody = (Event) response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(responseBody);
+        assertEquals(event.getId(), responseBody.getId());
+    }
+
+    @Test
+    void shouldThrowEventNotFoundException_whenThereIsNoEventWithTheSlug() {
+        var slug = "non-existing-slug";
+
+        var response = eventsController.detailBySlug(slug);
+        var responseBody = (ErrorMessageDTO) response.getBody();
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(responseBody);
+        assertEquals(String.format("Event with slug %s not found", slug), responseBody.message());
+    }
+}
